461cd4b385a5d3502cea35bda28f7f1e11eebf8b
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-03-25 02:25:39 +0530

    Login Page Creation Success

---
+++ components/FormField.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  FormControl,
+  FormDescription,
+  FormLabel,
+  FormItem,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  lable: string;
+  placeholder?: string;
+  type: "text" | "email" | "password" | "file";
+}
+
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  lable,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{lable}</FormLabel>
+        <FormControl>
+          <Input
+            className="input"
+            placeholder={placeholder}
+            type={type}
+            {...field}
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormField;
