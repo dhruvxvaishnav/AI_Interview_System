9f8806c77606ffe639c0b925148465891eaf697f
dhruvxvaishnav <dhruvvaishnav687@gmail.com>
2025-03-25 03:36:24 +0530

    Home Page Creation Initialisation

--- constant/index.ts
+++ constant/index.ts
@@ -202,3 +202,28 @@
 //   "/tiktok.png",
 //   "/yahoo.png",
 // ];
+
+export const dummyInterviews: Interview[] = [
+  {
+    id: "1",
+    userId: "user1",
+    role: "Frontend Developer",
+    type: "Technical",
+    techstack: ["React", "TypeScript", "Next.js", "Tailwind CSS"],
+    level: "Junior",
+    questions: ["What is React?"],
+    finalized: false,
+    createdAt: "2024-03-15T10:00:00Z",
+  },
+  {
+    id: "2",
+    userId: "user1",
+    role: "",
+    type: "",
+    techstack: [],
+    level: "",
+    questions: [],
+    finalized: false,
+    createdAt: "2024-05-15T12:00:00Z",
+  },
+];
